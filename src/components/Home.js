059bfa7bb2ccda977bcaf11d09186900542fc0d3
rasiamah <rasia003@odu.edu>
2022-09-15 22:36:03 -0400

    update on home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import rosby1 from "../images/Rosby1.jpg";
 import engbuilding from "../images/Engbuilding.jpg";
 import Box from "@mui/material/Box";
-// import { FormatQuoteRounded } from "@mui/icons-material";
+import { FormatQuoteRounded } from "@mui/icons-material";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -23,44 +23,61 @@ export default function Home() {
         </iframe>
         </Box> */}
                 
-      <div class=" grid grid-cols-2 bg-black container rounded max-w-full shadow-2xl mb-6 "> 
-        <Box class=" p-12 bg-white grid place-items-center ">
-          <div class="p-15 text-3xl bg-black text-center  bg-gray-300 rounded">
-            Motto: "Pomaja Twaweza"
+      <div class=" grid grid-cols-2 container rounded max-w-full mb-6 "> 
+        <Box class=" p-12 grid place-items-center ">
+          <div class="p-5 text-3xl font-bold text-center font-bold shadow-4xl bg-gray-300 rounded">
+            <span class="text-blue-900">Motto:</span> "Pomaja Twaweza"
           </div>
-          <div class="p-5 rounded  text-center text-justify bg-black text-white ">
+          <div class="p-5 rounded font-bold text-center text-justify shadow-4xl bg-black text-white italic">
             <strong>Mission:  </strong>Developing and connecting first-generation Black students
                      through mentoring for future success.
           </div>
-          <div class="p-5 rounded  text-center text-justify bg-gray-300 ">
+          <div class="p-5 rounded font-bold text-center text-justify shadow-2xl bg-gray-300 italic">
             <strong>Vission: </strong>To build intellectuals with confidence and equip them with 
                 skills needed to make a difference in the future.
           </div>
-          <div class="p-5 rounded   text-center text-justify bg-black text-white">
+          <div class="p-5 rounded font-bold text-center text-justify shadow-4xl bg-black text-white italic">
             <strong>Values: </strong>Academic Excellence, Moral Integrity, Selflessness, 
               Servitude, Sportsmanship, Fellowship, Discipline, Accountability, and Commitment.
           </div>
         </Box>
 
         <Box class="relative bg-black rounded ">
-          <img src={engbuilding} alt="engbuilding" class="top-6 w-full"/>
+          <img src={engbuilding} alt="engbuilding" class="top-6 rounded w-full"/>
+        </Box>
+
+      </div>
+      <div class="p-12">
+        <h1 class="text-center font-bold text-6xl">Our<span class="text-blue-900"> Mission</span></h1>
+        <Box class="text-black text-center">
+          <p class="mb-4 text-2xl text-justify pr-80 pl-80">
+              Relationships shape the experience and outcomes of a student’s journey through higher education. 
+              Some students will find these relationships among faculty and advisors; however, many more will 
+              struggle to forge the long-lasting relationships that drive student success and open doors. 
+          </p>
+          <p class="mb-4 text-2xl text-justify pr-80 pl-80">
+              At Mentor Collective, we seek to make life-changing relationships a feature of every student’s college experience. 
+              To realize this vision, we partner with forward-thinking institutions that are committed to equity, inclusion, and 
+              relationship-centered education.
+          </p>
+
         </Box>
 
       </div>
     
-      <div class=" grid grid-cols-2 max-w-full shadow-2xl ">
+      <div class=" grid grid-cols-2 max-w-full pt-16 shadow-2xl ">
       <Box class="relative bg-black ">
            <img src={rosby1} alt="rosby1" class="rounded"/> 
           <div class="absolute p-4 bg-black bg-opacity-40 top-1/2" >
-            {/* <div class=" text-white max-w-full italic text-justify" >
-            <div class=" text-center" > <FormatQuoteRounded/> </div>
+            <div class=" text-white max-w-full italic text-justify" >
+            <div class=" text-center leading-loose" > <FormatQuoteRounded/> </div>
             Being a member of the summit helped me find my feet at old dominion University when I first go there.
             It gave me the opportunit to meet friends who were willing to put in the effort and do what it takes
             to be successful and so that made me work harder. The friendliness and mentorship of the leaders motivated
              me alot. I'm glad to have been 
             a member of the summit. 
             <div class=" text-right " > "Rosby Asiamah" </div>
-            </div> */}
+            </div>
           </div>
         </Box>
 
